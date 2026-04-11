730fec9a3d75c3ae578756c58297bf3d01e09f54
paso22 <datopaska@gmail.com>
2026-04-11 13:40:41 +0400

    Initial commit

---
+++ src/main/java/ge/paso22/fileprocessingapp/FileProcessingAppApplication.java
@@ -0,0 +1,13 @@
+package ge.paso22.fileprocessingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileProcessingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ge/paso22/fileprocessingapp/FileProcessingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.paso22.fileprocessingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
